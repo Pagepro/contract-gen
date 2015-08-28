b1d4d84be8ac71c03316aa3c366277adb23edaa1
Dariusz Niemczyk <palid@o2.pl>
2015-08-28 13:24:33 +0200

    Still need to fix generator, but added config creator.

---
+++ app/generator.js
@@ -0,0 +1,131 @@
+'use strict';
+
+const fs = require('fs');
+const _ = require('lodash');
+const Decimal = require('decimal.js');
+const csv = require('csv');
+const markdownpdf = require('markdown-pdf');
+
+const path = require('path');
+
+const stringUtils = require('./util/string-utils.js');
+const config = require('../config.json');
+
+const q = require('q');
+const dfd = q.defer();
+
+const MARKDOWN_PDF_OPTIONS = {
+  // phantomPath: './node_modules/phantomjs2/bin/phantomjs',
+  cssPath: './css/pdf.css',
+  paperFormat: 'a4',
+  paperOrientation: 'portrait',
+  remarkable: {
+    html: true, // Enable HTML tags in source
+    xhtmlOut: false, // Use '/' to close single tags (<br />)
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    langPrefix: 'language-', // CSS language prefix for fenced blocks
+    linkify: true, // Autoconvert URL-like text to links
+    plugins: []
+  }
+};
+
+const TPL_PATH = './tpl';
+const CSV_PATH = '../csv';
+const RENDERED_PATH = '../rendered';
+
+function removeGitIgnore(item) {
+  return item !== '.gitignore';
+}
+
+const templates = _.filter(fs.readdirSync(path.join(__dirname, TPL_PATH)), removeGitIgnore);
+const data = _.filter(fs.readdirSync(path.join(__dirname, CSV_PATH)), removeGitIgnore);
+
+const precompiledTemplates = _.map(templates, function(item) {
+  let stringifiedTpl = fs.readFileSync(path.join(__dirname, `${TPL_PATH}/${item}`), {
+    encoding: 'utf-8'
+  });
+  return {
+    tpl: _.template(stringifiedTpl),
+    name: item,
+    string: stringifiedTpl
+  };
+});
+
+function transformCsvToObject(csvData, fileName) {
+  let keys = csvData[0];
+  let values = csvData[1];
+  let index = 0;
+  let responseObj = _.extend({}, config, {
+    fileName: fileName
+  });
+  if (keys.length !== values.length) {
+    throw new Error('CSV is badly formatted. There is some data missing.');
+  } else {
+    while (keys.length > index) {
+      let keyName = keys[index];
+      let clearKeyName = _.camelCase(stringUtils.replacer(keyName));
+      responseObj[clearKeyName] = values[index];
+      index++;
+    }
+  }
+
+  console.log(responseObj);
+  return responseObj;
+}
+
+var templateHelpers = {
+  getDecimal: function(kwotaBrutto) {
+    return new Decimal(parseFloat(kwotaBrutto.replace(',', '.').replace(/[  ]/g, '')));
+  },
+  kosztyUzyskania: function(kwotaBrutto) {
+    var decimal = templateHelpers.getDecimal(kwotaBrutto);
+    return decimal.times(0.5).round();
+  },
+  dochod: function(kwotaBrutto) {
+    var decimal = templateHelpers.getDecimal(kwotaBrutto);
+    return decimal.minus(templateHelpers.kosztyUzyskania(kwotaBrutto));
+  },
+  naleznyPodatek: function(kwotaBrutto) {
+    var dochod = templateHelpers.dochod(kwotaBrutto);
+    return dochod.times(config.podatek);
+  },
+  doWyplaty: function(kwotaBrutto) {
+    var decimal = templateHelpers.getDecimal(kwotaBrutto);
+    return decimal.minus(templateHelpers.naleznyPodatek(kwotaBrutto));
+  }
+};
+
+let stats = 0;
+
+function renderMarkdownTemplates(dataObject) {
+  return _.each(precompiledTemplates, function(item) {
+    let compiled = item.tpl(_.extend(dataObject, templateHelpers));
+    let filePath = path.join(__dirname, `${RENDERED_PATH}/${dataObject.fileName}-${item.name}`).replace('.md', '.pdf');
+
+    markdownpdf(MARKDOWN_PDF_OPTIONS).from.string(compiled).to(filePath, function() {
+      console.log("%s, was generated.", filePath);
+      stats++;
+      if (stats === data.length) {
+        dfd.resolve();
+      }
+    });
+  });
+}
+
+_.forEach(data, function(csvItem) {
+  const csvFile = fs.readFileSync(path.join(__dirname, `${CSV_PATH}/${csvItem}`), {
+    encoding: 'utf-8'
+  });
+  csv.parse(csvFile, {
+    comment: '#',
+    delimiter: ';'
+  }, function(err, output) {
+    if (err) {
+      console.log(err);
+    }
+    let dataObject = transformCsvToObject(output, csvItem.replace('.csv', ''));
+    renderMarkdownTemplates(dataObject);
+  });
+});
+
+module.exports = dfd.promise;
---
+++ app/prompt.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const prompt = require('prompt');
+const fs = require('fs');
+const path = require('path');
+const stats = fs.existsSync('./config.json');
+
+const q = require('q');
+const dfd = q.defer();
+const properties = [{
+  name: 'taxrate',
+  description: 'Taxrate (in decimal)',
+  validator: /[\d\.]+/,
+  required: true
+}, {
+  name: 'company',
+  description: 'Company name',
+  required: true
+}, {
+  name: 'firstName',
+  description: 'First name',
+  required: true
+}, {
+  name: 'surname',
+  description: 'Surname',
+  required: true
+}, {
+  name: 'street',
+  description: 'Street and number',
+  required: true
+}, {
+  name: 'zipcode',
+  description: 'Zip code',
+  required: true
+}, {
+  name: 'town',
+  description: 'Town',
+  required: true
+}, {
+  name: 'nip',
+  description: 'NIP',
+  required: true
+}, {
+  name: 'pesel',
+  description: 'PESEL',
+  required: true
+}, {
+  name: 'region',
+  description: 'Regon',
+  required: true
+}];
+
+function startPrompt() {
+  console.log("Starting genrator. Please enter required data.")
+  prompt.get(properties, function(err, result) {
+    if (err) {
+      console.log("There was an error. Please try again.")
+      console.log(err);
+    } else {
+      let obj = {
+        podatek: result.taxrate,
+        wystawca: {
+          firma: result.company,
+          imie: result.firstName,
+          nazwisko: result.surname,
+          ulica: result.street,
+          kodPocztowy: result.zipcode,
+          miejscowosc: result.town,
+          nip: result.nip,
+          pesel: result.pesel,
+          regon: result.regon
+        }
+      };
+      console.log(result);
+      console.log("Are above data valid? [Y/n]");
+      prompt.get({
+        name: 'isValid',
+        description: '[Y/n]',
+        validator: /[yn]{1}/i
+      }, function(validationErr, validationResult) {
+        if (validationResult.isValid.toLowerCase() === 'n') {
+          startPrompt();
+        } else {
+          console.log('Writing config to config.json.');
+          try {
+            fs.writeFileSync(path.join(__dirname, '..', '/config.json'), JSON.stringify(obj));
+            dfd.resolve();
+            console.log('File config.json successfully created at root directory.');
+          } catch (e) {
+            dfd.reject();
+            console.log('Couldn\'t write config.json.');
+          }
+        }
+      });
+    }
+  });
+}
+
+
+if (!stats) {
+  prompt.start();
+  console.log("File config.json wasn't found in the directory.")
+  startPrompt();
+} else {
+  dfd.resolve();
+}
+
+
+module.exports = dfd.promise;
